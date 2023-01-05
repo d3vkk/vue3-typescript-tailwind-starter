af5e8a28d20445c3f4fba00a26015fc2e4f0bd3f
d3vkk <donaldkk22@yahoo.com>
2023-01-05 15:06:44 +0300

    refactor: Replace vuex with pinia

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from "vue";
 import router from "./router/index";
-import { store } from "./store/index";
+import { stores } from "./stores/index";
 import App from "./App.vue";
 import "./assets/css/index.css";
 
 const app = createApp(App);
 app.use(router);
-app.use(store);
+app.use(stores);
 app.mount("#app");
